26f312509dab2505544f03cfd0dee42ecc48c965
Stanley Okoye <oakstanley@yahoo.com>
2023-08-30 11:54:11 +0200

    first commit part4.9

---
+++ models/blog.js
@@ -0,0 +1,32 @@
+/*eslint-disable*/
+
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: [true, 'title required']
+    },
+    author: {
+        type: String,
+        required: [true, 'author required']
+    },
+    url: String,
+    likes: Number
+
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+
+    }
+
+})
+
+
+
+module.exports = mongoose.model('Blog', blogSchema)
